60208748040c7bfe2ea1f420f14015f63a517e2b
ProstoGio <raviabaaravi@gmail.com>
2026-06-27 13:01:09 +0400

    more problems to easy category

---
+++ solutions/easy/085. გამრავლება/sol.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int sum, a, b;
+	cin>>a>>b;
+	sum=a*b;
+	cout<<sum;
+    
+    return 0;
+}
---
+++ solutions/easy/247. უდიდესი ციფრი/sol.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int a, b, c;
+	cin>>a;
+	b=a/10;
+	c=a%10;
+	if(b>c)cout<<b;
+	 else cout<<c;
+
+    return 0;
+}
